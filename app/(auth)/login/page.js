ad6725a7ddbf5dca6699534400de5fe05d1e8310
AnguloJuan <jua4_20@alu.uabcs.mx>
2023-06-10 16:22:20 -0700

    fix: login appearance

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -5,17 +5,17 @@ import Link from "next/link";
 export default function Adopcion() {
     return (
         <>
-            <h1>Iniciar sesión</h1>
-            <>
-                <Image
-                    src={"/images/logo.png"}
-                    alt='logo.png'
-                    width={400}
-                    height={250}
-                    priority={true}
-                />
-            </>
             <form action="Post">
+                <h1>Iniciar sesión</h1>
+                <>
+                    <Image
+                        src={"/images/logo.png"}
+                        alt='logo.png'
+                        width={400}
+                        height={250}
+                        priority={true}
+                    />
+                </>
                 <Input id={"email"} type={"email"} label={"Correo electrónico"} placeholder={"Correo electrónico"} />
                 <Input id={"password"} type={"password"} label={"Contraseña"} placeholder={"Contraseña"} />
                 <button type="submit" className="btn btn-primary mb-3">Iniciar sesión</button>
