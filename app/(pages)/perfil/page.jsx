bd0709ca9247d160fb4c0e7e22bd4a7967a7b4d0
alexisortega66 <mortega_20@alu.uabcs.mx>
2023-06-11 20:00:02 -0700

    feat: perfiles

---
+++ app/(pages)/perfil/page.jsx
@@ -0,0 +1,72 @@
+import InputLabel from "@/components/Input";
+import perfilAdoptador from "./perfil.module.css";
+
+
+export default function Perfil({ user }) {
+    return (
+        user == 1 ? (<>
+        <div className={perfilAdoptador}>
+            <h1>Perfil</h1>
+            <div className={perfilAdoptador.datosperfil}>
+                <InputLabel id={"imagen"} placeholder={"imagen/subir imagen"} />
+                <InputLabel id={"Nombre"} label={"Nombre"} placeholder={"nombre"} />
+                <InputLabel id={"correo"} label={"Correo electronico"} placeholder={"Correo electronico"} />
+                <InputLabel id={"numero"} label={"Numero de telefono"} placeholder={"Numero de telefono"} />
+                <InputLabel id={"ubicacion"} label={"Ubicacion"} placeholder={"Ubicacion"} />
+            </div>
+            <h3>Mascota adoptada</h3>
+            <div className={perfilAdoptador.container}>               
+                <div className={perfilAdoptador.fotoPerfil}></div>           
+                <div className={perfilAdoptador.datosMascotas}>
+                    <p>Nombre</p>
+                    <p>Especie</p>
+                    <p>Raza</p>
+                    </div>
+                
+                
+                <button type="submit">Guardar</button>
+                <button type="submit">Eliminar cuenta</button>
+                </div>
+                </div>
+   
+            </>
+            ) : user == 2 ? (<>
+               <form action="">
+                <div className="perfilEmpleado">
+                    <h1>Perfil</h1>
+                    <div className="datos-empleado">
+                        <InputLabel id={"imagen"} placeholder={"imagen/subir imagen"} />
+                        <InputLabel id={"Nombre"} label={"Nombre"} placeholder={"nombre"} />
+                        <InputLabel id={"correo"} label={"Correo electronico"} placeholder={"Correo electronico"} />
+                        <InputLabel id={"numero"} label={"Numero de telefono"} placeholder={"Numero de telefono"} />
+                        <InputLabel id={"ubicacion"} label={"Ubicacion"} placeholder={"Ubicacion"} />
+                        <InputLabel id={"tipoEmpleado"} label={"Tipo empleado"} placeholder={"Tipo empleado"} />
+
+                        <button type="submit">Guardar</button>
+                        </div>
+                        </div>
+            </form>/
+            </>
+            ) : user == 3 ? (
+                <>
+                    <form action="">
+                <div className="perfilEmpleado">
+                    <h1>Perfil</h1>
+                    <div className="datos-empleado">
+                        <InputLabel id={"imagen"} placeholder={"imagen/subir imagen"} />
+                        <InputLabel id={"Nombre"} label={"Nombre"} placeholder={"nombre"} />
+                        <InputLabel id={"correo"} label={"Correo electronico"} placeholder={"Correo electronico"} />
+                        <InputLabel id={"numero"} label={"Numero de telefono"} placeholder={"Numero de telefono"} />
+                        <InputLabel id={"ubicacion"} label={"Ubicacion"} placeholder={"Ubicacion"} />
+                        <InputLabel id={"tipoEmpleado"} label={"Tipo empleado"} placeholder={"Tipo empleado"} />
+
+                        <button type="submit">Guardar</button>
+                        </div>
+                        </div>
+            </form>/
+                </>
+            ) : (<>
+            </>
+            )
+    )
+}
