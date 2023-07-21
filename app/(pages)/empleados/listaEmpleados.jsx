11db804d532e03e73999e42d3b775c760fd073dd
AnguloJuan <jua4_20@alu.uabcs.mx>
2023-07-21 03:34:32 -0700

    Feat: Empleados agregado

---
+++ app/(pages)/empleados/listaEmpleados.jsx
@@ -0,0 +1,120 @@
+"use client"
+import InputLabel from "@/components/Input";
+import listaEmpleados from "./empleados.module.css"
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function ListaEmpleados({ props }) {
+    const [searchCriteria, setSearchCriteria] = useState({
+        id: "",
+        nombre: "",
+        tipoEmpleado: 0,
+        userId: props.user.id
+    });
+
+    const [empleados, setEmpleados] = useState([]);
+
+    useEffect(() => {
+        setEmpleados(props.empleados);
+    }, []);
+
+    const fetchEmpleado = async () => {
+        // Perform the API request to fetch the empleados list
+        try {
+            const search = JSON.stringify(searchCriteria);
+            const response = await fetch(`/api/empleado?search=${search}`);
+            if (response.ok) {
+                const data = await response.json();
+                setEmpleados(data.empleados);
+                //console.log(data.empleados);
+            } else {
+                console.error("Failed to fetch empleados");
+            }
+        } catch (error) {
+            console.error("An error occurred while fetching empleados", error);
+        }
+    };
+
+    // Function to handle input changes
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setSearchCriteria((prevCriteria) => ({ ...prevCriteria, [name]: value }));
+    };
+
+    return (
+        <>
+            <div>
+                <center><h2>Lista de empleados</h2></center>
+                <center><Link href={"empleados/registro"} className="btn btn-success border rounded mt-4 mb-2 d-flex align-items-center">
+                    <span className="f-bold fs-4 mx-2">+</span>Agregar nuevo empleado
+                </Link></center>
+                <div className={listaEmpleados.contenedor}>
+                    <div className={listaEmpleados.busqueda}>
+                        <InputLabel id={"empleado"} label={"ID del empleado"} placeholder={"Id empleado"} name={"id"} onChange={handleInputChange} />
+                    </div>
+                    <button
+                        className="btn btn-lg btn-success"
+                        onClick={fetchEmpleado}>Buscar</button>
+                </div>
+                <div className={listaEmpleados.contenedor}>
+                    <div className={listaEmpleados.busqueda}>
+                        <InputLabel id={"nombre"} label={"Nombre"} placeholder={"Nombre"} name={"nombre"} onChange={handleInputChange} />
+                    </div>
+                    <div className={listaEmpleados.busqueda}>
+                        <div className="input mb-3 mt-3">
+                            <label htmlFor="tipoEmpleado" className="form-label">Tipo empleado</label>
+                            <select
+                                id="tipoEmpleado"
+                                onChange={handleInputChange}
+                                name="tipoEmpleado"
+                                className="form-select">
+                                <option value="">Selecciona el tipo empleado</option>
+                                <option value={2}>Empleado</option>
+                                <option value={3}>Administrador</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {empleados.length !== 0 ?
+                <div className="d-flex flex-wrap">
+                    {empleados.map((empleado) => (
+                        <div key={empleado.id} className={`${listaEmpleados.tarjeta} bg-body-secondary rounded`}>
+                            <Link href={`/empleados/empleado/${empleado.id}`} className="link-underline-opacity-0">
+                                <div className={listaEmpleados.imagen}>
+                                    {empleado.imagen ? (
+                                        <Image
+                                            src={`/images/empleados/${empleado.imagen}`}
+                                            alt={`Imagen Adoptante${empleado.id}`}
+                                            width={300}
+                                            height={300}
+                                            loading="lazy"
+                                            color="white"
+                                        />
+                                    ) : (
+                                        <Image
+                                            src={"/images/defaultUser.png"}
+                                            alt='logo.png'
+                                            width={300}
+                                            height={300}
+                                            loading="lazy"
+                                            color="white"
+                                        />
+                                    )}
+                                </div>
+                                <div className={`${listaEmpleados.datos} rounded-bottom`}>
+                                    <p>Nombre: {empleado.nombre}</p>
+                                    <p>id: {empleado.id}</p>
+                                    <p>correo: {empleado.correo}</p>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+                : (<center><h3 className="mt-3">No se encontraron resultados</h3></center>)
+            }
+        </>
+    );
+}
---
+++ app/api/empleado/route.js
@@ -0,0 +1,205 @@
+import { getPrisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+import { v4 as uuid } from 'uuid';
+import { writeFile, unlink } from "fs/promises";
+import path from "path";
+import jwt from 'jsonwebtoken';
+import { GetUser } from "@/app/lib/user";
+
+const prisma = getPrisma();
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export async function POST(request) {
+    const { firstName, lastName, email, telefono, NIP, tipoEmpleado, password } = await request.json();
+    const numTelefono = parseInt(telefono);
+
+    try {
+        // Check if the email is already registered
+        const existingUser = await prisma.empleado.findUnique({ where: { correo: email } });
+
+        if (existingUser) {
+            return NextResponse.json({ message: 'Email already registered' }, { status: 409 });
+        }
+
+        if (!SECRET_KEY) {
+            return NextResponse.json({ message: 'Llave secreta no encontrada' }, { status: 500 })
+        }
+
+        // Hash the password
+        //const hashedPassword = await bcrypt.hash(password, 10);
+
+        let user;
+        const date = new Date().toISOString();
+        const idRefugio = GetUser().idRefugio;
+
+        // Create the user in the database
+        try {
+            user = await prisma.empleado.create({
+                data: {
+                    idRefugio: parseInt(idRefugio),
+                    nombre: firstName,
+                    apellido: lastName,
+                    correo: email,
+                    telefono: numTelefono,
+                    contrasena: password,
+                    NIP: NIP,
+                    tipoUsuario: { connect: { id: parseInt(tipoEmpleado) } },
+                    fechaRegistro: date,
+                },
+            });
+        } catch (e) {
+            console.log(e);
+        }
+        BigInt.prototype.toJSON = function () { return this.toString() }
+
+        return NextResponse.json({ message: 'User registered', user }, { status: 201 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
+    }
+}
+
+export async function GET(req) {
+    try {
+        let empleados;
+        const search = req.nextUrl.searchParams.get('search');
+        const { id, nombre, tipoEmpleado, userId } = JSON.parse(search);
+        try {
+            empleados = await prisma.empleado.findMany({
+                where: {
+                    id: id ? { equals: parseInt(id) } : undefined,
+                    nombre: nombre ? { contains: nombre } : undefined,
+                    idTipoUsuario: tipoEmpleado ? { equals: parseInt(tipoEmpleado) } : undefined,
+                    NOT: {
+                        id: userId,
+                    },
+                },
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        BigInt.prototype.toJSON = function () { return this.toString() }
+        return NextResponse.json({ empleados }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        NextResponse.json({ error: 'Ocurrio un fallo al realizar la busqueda' }, { staus: 500 });
+    }
+}
+
+export async function PUT(req) {
+    try {
+
+        const formData = await req.formData();
+        const userInit = formData.get("userInit");
+        const user = formData.get("user");
+        const userType = formData.get("userType");
+        const userInitParsed = JSON.parse(userInit.substring(userInit.indexOf('{'), userInit.lastIndexOf('}') + 1));
+        const userParsed = JSON.parse(user.substring(user.indexOf('{'), user.lastIndexOf('}') + 1));
+
+        const { correo, telefono } = userParsed;
+        let nombre, apellido, NIP, fechaRegistro, tipoEmpleado;
+        let date;
+        let image, uniqueName;
+        if (userType == 3) {
+            ({ nombre, apellido, NIP, fechaRegistro, tipoEmpleado } = userParsed);
+            date = new Date(Date.parse(fechaRegistro)).toISOString();
+
+            image = formData.get("image");
+            if (image !== "null") {
+                const bytes = await image.arrayBuffer();
+                const buffer = Buffer.from(bytes);
+                uniqueName = `${uuid()}.${image.name.split('.').pop()}`;
+                const filePath = path.join(process.cwd(), "public/images/empleados", uniqueName);
+                writeFile(filePath, buffer);
+
+                if (userInitParsed.imagen) {
+                    const oldFilePath = path.join(process.cwd(), "public/images/empleados", userInitParsed.imagen);
+
+                    // Elimina la imagen anterior utilizando fs/promises
+                    await unlink(oldFilePath);
+                }
+            }
+        }
+
+        //validate correo
+        if (correo !== userInitParsed.correo) {
+            const existingUser = await prisma.empleado.findUnique({ where: { correo: correo } });
+            if (existingUser) {
+                return NextResponse.json({ message: 'Email already registered' }, { status: 409 });
+            }
+        }
+
+        try {
+            let empleado;
+            userType == 2 ? (
+                empleado = await prisma.empleado.update({
+                    where: {
+                        id: parseInt(userInitParsed.id),
+                    },
+                    data: {
+                        correo: correo !== userInitParsed.correo ? correo : undefined,
+                        telefono: telefono !== userInitParsed.telefono ? parseInt(telefono) : undefined,
+                    }
+                })
+            ) : userType == 3 && (
+                empleado = await prisma.empleado.update({
+                    where: {
+                        id: parseInt(userInitParsed.id),
+                    },
+                    data: {
+                        nombre: nombre !== userInitParsed.nombre ? nombre : undefined,
+                        apellido: apellido !== userInitParsed.apellido ? apellido : undefined,
+                        correo: correo !== userInitParsed.correo ? correo : undefined,
+                        telefono: telefono !== userInitParsed.telefono ? parseInt(telefono) : undefined,
+                        NIP: NIP !== userInitParsed.NIP ? NIP : undefined,
+                        fechaRegistro: fechaRegistro !== userInitParsed.fechaRegistro ? date : undefined,
+                        idTipoUsuario: tipoEmpleado !== userInitParsed.idTipoUsuario ? parseInt(tipoEmpleado) : undefined,
+                        imagen: image !== "null" ? uniqueName : undefined
+                    }
+                })
+            );
+            //replacing user cookie
+            BigInt.prototype.toJSON = function () { return this.toString() }
+            const token = jwt.sign(empleado, SECRET_KEY);
+            return NextResponse.json({ message: 'User modified', empleado, token }, { status: 200 });
+        } catch (error) {
+            console.log(error);
+            return NextResponse.json({ message: 'Modification failed' }, { status: 500 });
+        }
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: 'Fields Failed' }, { status: 500 });
+    }
+}
+
+export async function DELETE(req) {
+    const id = req.nextUrl.searchParams.get('id');
+
+    try {
+        //delete image
+        const imagenPath = await prisma.empleado.findUnique({
+            where: {
+                id: parseInt(id)
+            },
+            select: {
+                imagen: true,
+            }
+        });
+        if (imagenPath.imagen !== null) {
+            const oldFilePath = path.join(process.cwd(), "public/images/empleados", imagenPath.imagen);
+            await unlink(oldFilePath);
+        }
+
+        //delete user
+        await prisma.empleado.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+
+        return NextResponse.json({ message: "Mascota eliminada" }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        NextResponse.json({ error: 'Ocurrio un fallo al realizar la busqueda' }, { staus: 500 });
+    }
+}
