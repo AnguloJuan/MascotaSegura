e0838d37913455dd62207b76dc6447e35c2f76ba
AnguloJuan <jua4_20@alu.uabcs.mx>
2024-04-02 19:30:53 -0700

    Reverted middleware to commit 497dde7

--- middleware.js
+++ middleware.js
@@ -3,8 +3,12 @@ import { GetUser } from '@/app/lib/user';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req) {
-	const user = GetUser();
-	console.log(user);
+	// Get user from the api request /api/user
+	const url = new URL('/api/user', req.url);
+	const response = await fetch(url, {
+		headers: req.headers,
+	}).then((res) => res.json());
+	const user = response.user;
 
 	if (user.idTipoUsuario == 0) {
 		return NextResponse.redirect(new URL('/login', req.url));
