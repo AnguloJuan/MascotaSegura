ebb83d7b67a341739ed9e1a5e7a32d3624606fe0
AnguloJuan <jua4_20@alu.uabcs.mx>
2024-04-04 19:33:25 -0700

    Refactor middleware.js for user authentication and access control

--- middleware.js
+++ middleware.js
@@ -1,17 +1,25 @@
 import { NextResponse } from 'next/server';
 import { GetUser } from '@/app/lib/user';
 
-const user = GetUser();
-export async function middleware(req) {
-	console.log(user);
+export async function middleware(request) {
+	const url = new URL('/api/user', request.url);
+	const response = await fetch(url, {
+		headers: request.headers,
+	}).then((res) => res.json());
+	const user = response.user;
 
-	if (user.idTipoUsuario == 0) {
-		return NextResponse.redirect(new URL('/login', req.url));
-	} else {
-		return NextResponse.next();
-	}
+	if (user.idTipoUsuario == 0) return NextResponse.redirect(new URL('/login', request.url));
+	if (request.nextUrl.pathname.startsWith('/adoptantes') && user.idTipoUsuario !== 3) return NextResponse.redirect(new URL('/not-found', request.url));
+	if (request.nextUrl.pathname.startsWith('/empleados') && user.idTipoUsuario !== 3) return NextResponse.redirect(new URL('/not-found', request.url));
+	if (request.nextUrl.pathname.startsWith('/rescate') && (user.idTipoUsuario !== 3 || user.idTipoUsuario !== 2)) return NextResponse.redirect(new URL('/not-found', request.url));
+	return NextResponse.next();
 }
 
 export const config = {
-	matcher: '/adopcion/mascota/:path*',
+	matcher: [
+		'/adopcion/mascota/:path*',
+		'/adoptantes/:path*',
+		'/empleados/:path*',
+		'/rescate/:path*',
+	]
 };
